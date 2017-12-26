49f4c12a97dbe4d3d05ec71d53d141ca77d79136
Phillip Lord <phillip.lord@russet.org.uk>
2017-12-26 16:22:07 +0000

    Add more tests

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,22 @@
-extern crate horned_owl_index;
+extern crate horned_owl;
 
 
-use horned_owl_index::*;
-
+use horned_owl::*;
 
 pub fn main(){
-
     let mut o = MutableOntology::new();
 
     let i = o.iri("http://example.com/c".to_string());
-
-    println!("i is {:?}", i);
-
     let c = o.class(i);
-
-    println!("c is {:?}", c);
-
     let i = o.iri("http://example.com/d".to_string());
     let d = o.class(i);
 
-    let sc = o.subclass(c,d);
-    
-    println!("sc is {:?}", sc);
-    let dsc = o.direct_subclass(c);
+    let i = o.iri("http://example.com/r".to_string());
+    let ob = o.object_property(i);
 
-    println!("dsc is {:?}", dsc);
+    let some = o.some(ob, d);
+    let se = o.subclass_exp(ClassExpression::Class(c),some);
+    println!("{:?}",se);
 
-    println!("is_subclass {:?}", o.is_subclass(c, d));
 
-    let iri = o.iri("http://example.com/r".to_string());
-    let op = o.object_property(iri);
-    let some = o.some(op,d);
-    let sc2 = o.subclass_exp(ClassExpression::Class(c), some.clone());
-    println!("some is {:?}", some);
-    let dsc = o.direct_subclass(c);
-    println!("dsc is {:?}", dsc);
 }
