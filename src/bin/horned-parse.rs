1598528175b55a76dbd5fa77fba51e2bfb7f1258
Phillip Lord <phillip.lord@russet.org.uk>
2018-06-15 23:07:17 +0100

    Add horned-parse command line tool

---
+++ src/bin/horned-parse.rs
@@ -0,0 +1,32 @@
+extern crate clap;
+extern crate horned_owl;
+
+use clap::App;
+use clap::Arg;
+
+use horned_owl::io::reader::read;
+
+use std::io::BufReader;
+use std::fs::File;
+
+fn main() {
+    let matches =
+        App::new("horned-parse")
+        .version("0.1")
+        .about("Parse an OWL File")
+        .author("Phillip Lord")
+        .arg(Arg::with_name("INPUT")
+             .help("Sets the input file to use")
+             .required(true)
+             .index(1))
+        .get_matches();
+
+
+    let input = matches.value_of("INPUT").unwrap();
+
+    let file = File::open(input).unwrap();
+    let mut bufreader = BufReader::new(file);
+    let (_,_) = read(&mut bufreader);
+
+    println!("Parse Complete: {:?}", input);
+}
