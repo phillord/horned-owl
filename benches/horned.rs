153817358bd822251a3156875156a028e1924fa9
Phillip Lord <phillip.lord@russet.org.uk>
2022-03-29 22:06:10 +0100

    Update benches

--- benches/horned.rs
+++ benches/horned.rs
@@ -9,7 +9,7 @@ use bencher::Bencher;
 
 fn a_thousand_classes(bench: &mut Bencher) {
     bench.iter(|| {
-        let b = Build::default();
+        let b = Build::new_rc();
         let mut o = SetOntology::new();
         for m in 1..1000 {
             let i = b.iri(format!("http://example.com/b{}", m));
@@ -20,21 +20,21 @@ fn a_thousand_classes(bench: &mut Bencher) {
 
 fn big_tree(bench: &mut Bencher) {
     bench.iter(|| {
+        let b = Build::new_rc();
         let mut o = SetOntology::new();
         let mut i = 10_000;
-        create_tree(&mut o, &mut i);
+        create_tree(&b, &mut o, &mut i);
     })
 }
 
-fn create_tree(o: &mut SetOntology, n: &mut i32) {
-    let b = Build::default();
+fn create_tree<A:ForIRI>(b: &Build<A>, o: &mut SetOntology<A>, n: &mut i32) {
+
     let i = b.iri(format!("http://example.com/a{}", n));
     let c = b.class(i);
-    create_tree_0(o, vec![c], n);
+    create_tree_0(b, o, vec![c], n);
 }
 
-fn create_tree_0(o: &mut SetOntology, current: Vec<Class>, remaining: &mut i32) {
-    let b = Build::default();
+fn create_tree_0<A:ForIRI>(b:&Build<A>, o: &mut SetOntology<A>, current: Vec<Class<A>>, remaining: &mut i32) {
     let mut next = vec![];
 
     for curr in current.into_iter() {
@@ -61,54 +61,87 @@ fn create_tree_0(o: &mut SetOntology, current: Vec<Class>, remaining: &mut i32) 
             return;
         }
     }
-    create_tree_0(o, next, remaining);
+    create_tree_0(b, o, next, remaining);
 }
 
-fn is_subclass_with_many_direct_subclasses(bench: &mut Bencher) {
-    bench.iter(|| {
-        let b = Build::default();
-        let mut o = SetOntology::new();
-        let i = b.iri("http://example.com/a".to_string());
-        let c = b.class(i);
+// fn is_subclass_with_many_direct_subclasses(bench: &mut Bencher) {
+//     bench.iter(|| {
+//         let b = Build::default();
+//         let mut o = SetOntology::new();
+//         let i = b.iri("http://example.com/a".to_string());
+//         let c = b.class(i);
 
-        let n = 1_000;
-        for m in 1..n {
-            let i = b.iri(format!("http://example.com/b{}", m));
-            let d = b.class(i);
-            o.insert(SubClassOf::new(
-                ClassExpression::Class(c.clone()),
-                ClassExpression::Class(d.clone()),
-            ));
-        }
+//         let n = 1_000;
+//         for m in 1..n {
+//             let i = b.iri(format!("http://example.com/b{}", m));
+//             let d = b.class(i);
+//             o.insert(SubClassOf::new(
+//                 ClassExpression::Class(c.clone()),
+//                 ClassExpression::Class(d.clone()),
+//             ));
+//         }
 
-        let i = b.iri(format!("http://example.com/b{}", n - 1));
-        let d = b.class(i);
-        o.declare(d.clone());
+//         let i = b.iri(format!("http://example.com/b{}", n - 1));
+//         let d = b.class(i);
+//         o.declare(d.clone());
 
-        assert!(!o.is_subclass(&d, &c));
-        assert!(o.is_subclass(&c, &d));
-    })
-}
+//         assert!(!o.is_subclass(&d, &c));
+//         assert!(o.is_subclass(&c, &d));
+//     })
+// }
 
 benchmark_group!(
     benches,
     a_thousand_classes,
     big_tree,
-    is_subclass_with_many_direct_subclasses
+//    is_subclass_with_many_direct_subclasses
 );
 
+
+use curie::PrefixMapping;
 use std::fs::File;
 use std::io::BufReader;
+use std::rc::Rc;
 
 fn io_read(bench: &mut Bencher) {
     bench.iter(|| {
         let f = File::open("benches/ont/o100.owl").ok().unwrap();
         let mut f = BufReader::new(f);
 
-        horned_owl::io::owx::reader::read(&mut f).ok();
+        let _: Option<(SetOntology<Rc<str>>, PrefixMapping)> = horned_owl::io::owx::reader::read(&mut f).ok();
     })
 }
 
+
+fn bigger_tree<A:ForIRI>(b: Build<A>){
+    let mut o = SetOntology::new();
+    let mut i = 50_000;
+    create_tree(&b, &mut o, &mut i);
+}
+
+fn big_tree_rc(bench: &mut Bencher) {
+    bench.iter(|| {
+        bigger_tree(Build::new_rc())
+    })
+}
+
+fn big_tree_arc(bench: &mut Bencher) {
+    bench.iter(|| {
+        bigger_tree(Build::new_arc())
+    })
+}
+
+fn big_tree_string(bench: &mut Bencher) {
+    bench.iter(|| {
+        let b:Build<String> = Build::default();
+        bigger_tree(b)
+    })
+}
+
+
+
+benchmark_group!(storebench, big_tree_rc, big_tree_arc, big_tree_string);
+
 benchmark_group!(iobenches, io_read);
 
-benchmark_main!(benches, iobenches);
+benchmark_main!(benches, iobenches, storebench);
